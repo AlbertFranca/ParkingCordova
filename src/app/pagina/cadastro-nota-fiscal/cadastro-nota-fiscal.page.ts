ec76b93936519ac25d68762864a190e7bb309a40
Albert França <albertdfranca@gmail.com>
2022-05-03 20:06:30 -0300

    first commit

---
+++ src/app/pagina/cadastro-nota-fiscal/cadastro-nota-fiscal.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cadastro-nota-fiscal',
+  templateUrl: './cadastro-nota-fiscal.page.html',
+  styleUrls: ['./cadastro-nota-fiscal.page.scss'],
+})
+export class CadastroNotaFiscalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
